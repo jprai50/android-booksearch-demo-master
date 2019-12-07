75d22643ef28474a86565542706b5ee7a267b941
Jai Prakash Rai <31644654+jprai50@users.noreply.github.com>
2019-12-07 10:33:05 +0530

    Removed depcrated test API

--- app/src/androidTest/java/com/codepath/android/booksearch/ApplicationTest.java
+++ app/src/androidTest/java/com/codepath/android/booksearch/ApplicationTest.java
@@ -1,13 +1,15 @@
 package com.codepath.android.booksearch;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.app.Activity;
+
+import android.support.test.rule.ActivityTestRule;
+
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ActivityTestRule<Activity> {
     public ApplicationTest() {
-        super(Application.class);
+        super(Activity.class);
     }
 }
